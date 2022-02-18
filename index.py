84469837621091aca193ed0a4f27e392abb13f13
marnixah <marnix@hagehoeve.com>
2022-02-18 21:08:22 +0100

    dev: 4 spaces -> 2 spaces

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ bot = GitLabBot(getenv("GL_USER"))
 # Register a callack for a webhook event
 @bot.router.register("Issue Hook", action="open")
 async def issue_opened_event(event, gl, *args, **kwargs):
-    url = f"/projects/{event.project_id}/issues/{event.object_attributes['iid']}/notes"
-    print(f"Posting to {url}")
-    message = f"Thanks for the report @{event.data['user']['username']}! I will look into it ASAP! (I'm a bot)."
-    await gl.post(url, data={"body": message})
+  url = f"/projects/{event.project_id}/issues/{event.object_attributes['iid']}/notes"
+  print(f"Posting to {url}")
+  message = f"Thanks for the report @{event.data['user']['username']}! I will look into it ASAP! (I'm a bot)."
+  await gl.post(url, data={"body": message})
 
 bot.run()
